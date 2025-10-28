2a0bae4fc47c8d13a62f035f9dbd98b3b4826210
Chayanon-Pond <chayanon.kmutnb@gmail.com>
2025-10-28 16:59:29 +0700

    refactor

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,10 +6,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "http://localhost:3000",
+ app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://seat-master.vercel.app',
+      /\.vercel\.app$/
+    ],
     credentials: true,
-  })
+  });
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
